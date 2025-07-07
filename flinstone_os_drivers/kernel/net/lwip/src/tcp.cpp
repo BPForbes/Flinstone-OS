0c5ac2ac700fb41099f5ff320840a2d5298b74f6
Bailey P Forbes <62630945+BPForbes@users.noreply.github.com>
2025-07-07 15:41:06 -0400

    Add network protocol headers and update sources

--- flinstone_os_drivers/kernel/net/lwip/src/tcp.cpp
+++ flinstone_os_drivers/kernel/net/lwip/src/tcp.cpp
@@ -1,11 +1,3 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <net/ethernet.h>
-#include <netinet/if_ether.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include "tcp.hpp"
 
 void tcp_connect(void* pcb, void* ipaddr, int port, void* cb) {}
--- flinstone_os_drivers/kernel/net/udp.hpp
+++ flinstone_os_drivers/kernel/net/udp.hpp
@@ -1,1 +1,16 @@
-// stub for UDP.h
+#ifndef UDP_H
+#define UDP_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+struct udphdr {
+    uint16_t source;
+    uint16_t dest;
+    uint16_t len;
+    uint16_t check;
+} __attribute__((packed));
+
+void udp_handle(uint8_t* data, size_t len);
+
+#endif
